4038eb5c5cf4dd7723d68e9d7510cab7ea4b1875
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-10-05 23:03:03 +0300

    register to admin new model Author

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from myapp.models import Post
+from myapp.models import Post, Author
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(Author)
