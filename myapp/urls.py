5b6c46f7a6a347f800c26185355f2307e7ab9438
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-10-05 23:04:14 +0300

    change in urls in myapp and my_site

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 import myapp.views as myapp_views
 
 urlpatterns = [
-    path("", myapp_views.post_list, name="post_list")
+    path("authors/", myapp_views.author_list, name="author_list"),
+    path("authors/<int:author_id>/", myapp_views.get_author_by_id, name="author_detail"),
+    path("posts/", myapp_views.post_list, name="posts_list"),
+    path("posts/<int:post_id>/", myapp_views.get_post_by_id, name="post_detail"),
+    path("test/", myapp_views.test, name="test"),
 ]
